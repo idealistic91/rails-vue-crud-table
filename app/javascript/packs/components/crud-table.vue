f0d921a1c57f1172687a56ea652ff3151a9169bc
idealistic91 <frerichs.jens@hotmail.de>
2021-10-28 22:15:59 +0200

    Install pug and pug-plain-loader; Add Loader to webpack environment; Convert crud-table to pug lang

--- app/javascript/packs/components/crud-table.vue
+++ app/javascript/packs/components/crud-table.vue
@@ -1,70 +1,46 @@
-<template>
-  <v-data-table :headers="headers" :items="items" :sort-by="config.defaultSortBy" class="elevation-2">
-    <template v-slot:top>
-      <v-toolbar flat color="white">
-        <!-- <v-toolbar-title>{{ config.label }}</v-toolbar-title> -->
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ on }">
-            <v-btn color="primary" dark class="mb-2" v-on="on">Neu</v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col v-for="attribute in attributes" :key="attribute.value" cols="12" sm="6" md="4">
-                      <!-- Needs logic to determine which kind of input is appropiate for attribute given -->
-                      <v-text-field v-model="editedItem[attribute.value]" :label="attribute.text"></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close">Abbrechen</v-btn>
-              <v-btn color="blue darken-1" text @click="save(editedItem)">Speichern</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">            
-          <v-card>
-               <v-alert class="rounded-0"
-                type="error"
-                >
-                    <v-row align="center">
-                        <v-col class="grow">
-                            Wirklich löschen?
-                        </v-col>
-                    </v-row>
-                </v-alert>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Abbrechen</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm">Löschen</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.action="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)">Bearbeiten</v-icon>
-      <v-icon small @click="deleteItem(item)">Löschen</v-icon>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize">Aktualisieren</v-btn>
-    </template>
-  </v-data-table>
+<template lang="pug">
+  v-data-table.elevation-2(:headers='headers', :items='items', :sort-by='config.defaultSortBy')
+    template(v-slot:top)
+      v-toolbar(flat, color='white')
+        v-divider.mx-4(inset='', vertical='')
+        v-spacer
+        v-dialog(v-model='dialog', max-width='500px')
+          template(v-slot:activator='{ on }')
+            v-btn.mb-2(color='primary', dark='', v-on='on') Neu
+          v-card
+            v-card-title
+              span.headline {{ formTitle }}
+            v-card-text
+              v-container
+                v-row
+                  v-col(v-for='attribute in attributes', :key='attribute.value', cols='12', sm='6', md='4')
+                    // Needs logic to determine which kind of input is appropiate for attribute given
+                    v-text-field(v-model='editedItem[attribute.value]', :label='attribute.text')
+            v-card-actions
+              v-spacer
+              v-btn(color='blue darken-1', text, @click='close') Abbrechen
+              v-btn(color='blue darken-1', text, @click='save(editedItem)') Speichern
+        v-dialog(v-model='dialogDelete', max-width='500px')
+          v-card
+            v-alert.rounded-0(type='error')
+              v-row(align='center')
+                v-col.grow
+                  | Wirklich l&ouml;schen?
+            v-card-actions
+              v-spacer
+              v-btn(color='blue darken-1', text, @click='closeDelete') Abbrechen
+              v-btn(color='blue darken-1', text, @click='deleteItemConfirm') L&ouml;schen
+              v-spacer
+    template(v-slot:item.action='{ item }')
+      v-icon.mr-2(small, @click='editItem(item)') Bearbeiten
+      v-icon(small, @click='deleteItem(item)') L&ouml;schen
+    template(v-slot:no-data)
+      v-btn(color='primary', @click='initialize') Aktualisieren
 </template>
 
 <script>
 import axios from "axios";
+  // Move axios to config, set default headers, etc. 
 export default {
   data: () => ({
     dialog: false,
